44d41162901a425a685f401cc1689e248ab9a54b
Anuragp22 <anuragp2003b@gmail.com>
2024-10-31 03:24:36 +0530

    dashboard layout

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -88,3 +88,28 @@ export const onAuthenticateUser = async () => {
     return { status: 500 };
   }
 };
+
+export const getNotifications = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) return { status: 404 };
+    const notifications = await client.user.findUnique({
+      where: { clerkid: user.id },
+      select: {
+        notifications: true,
+        _count: {
+          select: {
+            notifications: true,
+          },
+        },
+      },
+    });
+
+    if (notifications && notifications.notifications.length > 0) {
+      return { status: 200, data: notifications };
+    }
+    return { status: 404, data: [] };
+  } catch (error) {
+    return { status: 404, data: [] };
+  }
+};
--- src/app/dashboard/[workspaceId]/layout.tsx
+++ src/app/dashboard/[workspaceId]/layout.tsx
@@ -1,6 +1,16 @@
-import { onAuthenticateUser } from '@/actions/user'
+import { getNotifications, onAuthenticateUser } from '@/actions/user'
+import { getAllUserVideos, getWorkspaceFolders, getWorkSpaces, verifyAccessToWorkspace } from '@/actions/workspace'
 import { redirect } from 'next/navigation'
 import React from 'react'
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+    HydrationBoundary,
+    dehydrate,
+} from '@tanstack/react-query'
 
 type Props = {
     params: { workspaceId: string }
@@ -11,8 +21,46 @@ const Layout = async ({ params: { workspaceId }, children }: Props) => {
     const auth = await onAuthenticateUser()
     if (!auth.user?.workspace) redirect('/auth/sign-in')
     if (!auth.user?.workspace?.length) redirect('/auth/sign-in')
+    const hasAccess = await verifyAccessToWorkspace(workspaceId)
+    if (hasAccess.status !== 200) {
+        redirect(`/dashboard/${auth.user?.workspace[0].id}`)
+    }
+
+    if (!hasAccess.data?.workspace) return null
+
+    const query = new QueryClient()
+
+
+
+    await query.prefetchQuery({
+        queryKey: ['workspace-folders'],
+        queryFn: () => getWorkspaceFolders(workspaceId),
+    })
+    await query.prefetchQuery({
+        queryKey: ['user-videos'],
+        queryFn: () => getAllUserVideos(workspaceId),
+    })
+    await query.prefetchQuery({
+        queryKey: ['user-workspaces'],
+        queryFn: () => getWorkSpaces(),
+    })
+
+    await query.prefetchQuery({
+        queryKey: ['user-notifications'],
+        queryFn: () => getNotifications(),
+    })
+
+
     return (
-        <div>Layout</div>
+        <HydrationBoundary state={dehydrate(query)}>
+            <div className="flex h-screen w-screen">
+
+                <div className="w-full pt-28 p-6 overflow-y-scroll overflow-x-hidden">
+
+                    <div className="mt-4">{children}</div>
+                </div>
+            </div>
+        </HydrationBoundary>
     )
 }
 
